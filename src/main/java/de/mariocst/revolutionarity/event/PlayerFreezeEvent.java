4dacaa6fab649bbea9efda09289da3021f6cc847
marioCST <wiiumr@web.de>
2023-01-21 15:23:14 +0100

    WTF was my english

--- src/main/java/de/mariocst/revolutionarity/event/PlayerFreezeEvent.java
+++ src/main/java/de/mariocst/revolutionarity/event/PlayerFreezeEvent.java
@@ -6,22 +6,22 @@ import cn.nukkit.event.player.PlayerEvent;
 
 public class PlayerFreezeEvent extends PlayerEvent {
     private static final HandlerList handlers = new HandlerList();
-    private boolean isFreezed;
+    private boolean frozen;
 
     public static HandlerList getHandlers() {
         return handlers;
     }
 
-    public PlayerFreezeEvent(Player player, boolean isFreezed) {
+    public PlayerFreezeEvent(Player player, boolean frozen) {
         this.player = player;
-        this.isFreezed = isFreezed;
+        this.frozen = frozen;
     }
 
-    public boolean isFreezed() {
-        return isFreezed;
+    public boolean isFrozen() {
+        return frozen;
     }
 
-    public void setFreezed(boolean freezed) {
-        isFreezed = freezed;
+    public void setFrozen(boolean frozen) {
+        this.frozen = frozen;
     }
 }
